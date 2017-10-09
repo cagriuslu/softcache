76f758948c30830f759eeddf405ef77d183af1cf
Cagri Uslu <uslu.cagri@gmail.com>
2017-10-09 17:47:24 +0300

    Rewritten FifoCacheTest

--- test/FifoCacheTest.cc
+++ test/FifoCacheTest.cc
@@ -4,84 +4,88 @@
 #include <softcache/FifoCache.hh>
 #include <string>
 
-TEST_CASE("FifoCache")
+TEST_CASE("FifoCache", "[fifo] [cache] [container]")
 {
-	sc::FifoCache<int,std::string> cache(5);
+	sc::FifoCache<int,std::string> cache(2);
 	std::pair<int,std::string> pair;
 
-	// size = 0
-	REQUIRE(cache.capacity() == 5);
-	REQUIRE(cache.size() == 0);
-	REQUIRE(cache.empty() == true);
-	REQUIRE(cache.full() == false);
-	REQUIRE(cache.pull(pair) == false);
-	REQUIRE(cache.peak(pair) == false);
+	SECTION("size = 0")
+	{
+		REQUIRE(cache.capacity() == 2);
+		REQUIRE(cache.size() == 0);
+		REQUIRE(cache.empty() == true);
+		REQUIRE(cache.full() == false);
+		REQUIRE(cache.pull(pair) == false);
+		REQUIRE(cache.peak(pair) == false);
 
-	// size = 1
-	pair.first = 1;
-	pair.second = "first";
-	REQUIRE(cache.push(pair) == true);
-	REQUIRE(cache.capacity() == 5);
-	REQUIRE(cache.size() == 1);
-	REQUIRE(cache.empty() == false);
-	REQUIRE(cache.full() == false);
-	pair.second = "";
-	REQUIRE(cache.peak(pair) == true);
-	REQUIRE(pair.second == "first");
+		SECTION("size = 1")
+		{
+			pair.first = 1;
+			pair.second = "first";
+			REQUIRE(cache.push(pair) == true);
+			pair.second = "";
+			REQUIRE(cache.peak(pair) == true);
+			REQUIRE(pair.second == "first");
+			REQUIRE(cache.push(pair) == true);
 
-	// size = 0
-	pair.first = 0;
-	pair.second = "";
-	REQUIRE(cache.pull(pair) == true);
-	REQUIRE(pair.first == 1);
-	REQUIRE(pair.second == "first");
-	REQUIRE(cache.capacity() == 5);
-	REQUIRE(cache.size() == 0);
-	REQUIRE(cache.empty() == true);
-	REQUIRE(cache.full() == false);
-	REQUIRE(cache.pull(pair) == false);
-	REQUIRE(cache.peak(pair) == false);
+			REQUIRE(cache.capacity() == 2);
+			REQUIRE(cache.size() == 1);
+			REQUIRE(cache.empty() == false);
+			REQUIRE(cache.full() == false);
 
-	// size = 5
-	pair.first = 1;
-	pair.second = "first";
-	REQUIRE(cache.push(pair) == true);
-	pair.first = 2;
-	pair.second = "second";
-	REQUIRE(cache.push(pair) == true);
-	pair.first = 3;
-	pair.second = "third";
-	REQUIRE(cache.push(pair) == true);
-	pair.first = 4;
-	pair.second = "forth";
-	REQUIRE(cache.push(pair) == true);
-	pair.first = 5;
-	pair.second = "fifth";
-	REQUIRE(cache.push(pair) == true);
-	REQUIRE(cache.capacity() == 5);
-	REQUIRE(cache.size() == 5);
-	REQUIRE(cache.empty() == false);
-	REQUIRE(cache.full() == true);
+			pair.first = 0;
+			pair.second = "";
+			REQUIRE(cache.peak(pair) == false);
 
-	pair.first = 6;
-	pair.second = "sixth";
-	REQUIRE(cache.push(pair) == false);
-	pair.first = 3;
-	pair.second = "third";
-	REQUIRE(cache.push(pair) == true);
-	pair.first = 0;
-	pair.second = "";
-	REQUIRE(cache.peak(pair) == false);
-	pair.first = 4;
-	REQUIRE(cache.peak(pair) == true);
-	REQUIRE(pair.second == "forth");
-	pair.first = 0;
-	pair.second = "";
-	REQUIRE(cache.pull(pair) == true);
-	REQUIRE(pair.first == 1);
-	REQUIRE(pair.second == "first");
-	REQUIRE(cache.capacity() == 5);
-	REQUIRE(cache.size() == 4);
-	REQUIRE(cache.empty() == false);
-	REQUIRE(cache.full() == false);
+			SECTION("size = 0")
+			{
+				pair.first = 0;
+				pair.second = "";
+				REQUIRE(cache.pull(pair) == true);
+				REQUIRE(pair.first == 1);
+				REQUIRE(pair.second == "first");
+
+				REQUIRE(cache.capacity() == 2);
+				REQUIRE(cache.size() == 0);
+				REQUIRE(cache.empty() == true);
+				REQUIRE(cache.full() == false);
+				REQUIRE(cache.pull(pair) == false);
+				REQUIRE(cache.peak(pair) == false);
+			}
+
+			SECTION("size = 2")
+			{
+				pair.first = 2;
+				pair.second = "second";
+				REQUIRE(cache.push(pair) == true);
+				pair.second = "";
+				REQUIRE(cache.peak(pair) == true);
+				REQUIRE(pair.second == "second");
+				REQUIRE(cache.push(pair) == true);
+
+				REQUIRE(cache.capacity() == 2);
+				REQUIRE(cache.size() == 2);
+				REQUIRE(cache.empty() == false);
+				REQUIRE(cache.full() == true);
+
+				pair.first = 3;
+				pair.second = "third";
+				REQUIRE(cache.push(pair) == false);
+
+				SECTION("size = 1")
+				{
+					pair.first = 0;
+					pair.second = "";
+					REQUIRE(cache.pull(pair) == true);
+					REQUIRE(pair.first == 1);
+					REQUIRE(pair.second == "first");
+
+					REQUIRE(cache.capacity() == 2);
+					REQUIRE(cache.size() == 1);
+					REQUIRE(cache.empty() == false);
+					REQUIRE(cache.full() == false);
+				}
+			}
+		}
+	}
 }
