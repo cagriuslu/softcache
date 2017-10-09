6551c6708184069420397c67086cca6dd837b27c
Cagri Uslu <uslu.cagri@gmail.com>
2017-10-09 22:16:40 +0300

    Removed unnecessary indentation

--- test/FifoCacheTest.cc
+++ test/FifoCacheTest.cc
@@ -9,35 +9,68 @@ TEST_CASE("FifoCache", "[fifo] [cache] [container]")
 	sc::FifoCache<int,std::string> cache(2);
 	std::pair<int,std::string> pair;
 
-	SECTION("Size = 0")
+	REQUIRE(cache.capacity() == 2);
+	REQUIRE(cache.size() == 0);
+	REQUIRE(cache.empty() == true);
+	REQUIRE(cache.full() == false);
+	REQUIRE(cache.pull(pair) == false);
+	REQUIRE(cache.peak(pair) == false);
+
+	SECTION("Size = 1")
 	{
+		pair.first = 1;
+		pair.second = "first";
+		REQUIRE(cache.push(pair) == true);
+		pair.second = "";
+		REQUIRE(cache.peak(pair) == true);
+		REQUIRE(pair.second == "first");
+		REQUIRE(cache.push(pair) == true);
+
 		REQUIRE(cache.capacity() == 2);
-		REQUIRE(cache.size() == 0);
-		REQUIRE(cache.empty() == true);
+		REQUIRE(cache.size() == 1);
+		REQUIRE(cache.empty() == false);
 		REQUIRE(cache.full() == false);
-		REQUIRE(cache.pull(pair) == false);
+
+		pair.first = 0;
+		pair.second = "";
 		REQUIRE(cache.peak(pair) == false);
 
-		SECTION("Size = 1")
+		SECTION("Size = 0")
 		{
-			pair.first = 1;
-			pair.second = "first";
+			pair.first = 0;
+			pair.second = "";
+			REQUIRE(cache.pull(pair) == true);
+			REQUIRE(pair.first == 1);
+			REQUIRE(pair.second == "first");
+
+			REQUIRE(cache.capacity() == 2);
+			REQUIRE(cache.size() == 0);
+			REQUIRE(cache.empty() == true);
+			REQUIRE(cache.full() == false);
+			REQUIRE(cache.pull(pair) == false);
+			REQUIRE(cache.peak(pair) == false);
+		}
+
+		SECTION("Size = 2")
+		{
+			pair.first = 2;
+			pair.second = "second";
 			REQUIRE(cache.push(pair) == true);
 			pair.second = "";
 			REQUIRE(cache.peak(pair) == true);
-			REQUIRE(pair.second == "first");
+			REQUIRE(pair.second == "second");
 			REQUIRE(cache.push(pair) == true);
 
 			REQUIRE(cache.capacity() == 2);
-			REQUIRE(cache.size() == 1);
+			REQUIRE(cache.size() == 2);
 			REQUIRE(cache.empty() == false);
-			REQUIRE(cache.full() == false);
+			REQUIRE(cache.full() == true);
 
-			pair.first = 0;
-			pair.second = "";
-			REQUIRE(cache.peak(pair) == false);
+			pair.first = 3;
+			pair.second = "third";
+			REQUIRE(cache.push(pair) == false);
 
-			SECTION("Size = 0")
+			SECTION("Size = 1")
 			{
 				pair.first = 0;
 				pair.second = "";
@@ -46,45 +79,9 @@ TEST_CASE("FifoCache", "[fifo] [cache] [container]")
 				REQUIRE(pair.second == "first");
 
 				REQUIRE(cache.capacity() == 2);
-				REQUIRE(cache.size() == 0);
-				REQUIRE(cache.empty() == true);
-				REQUIRE(cache.full() == false);
-				REQUIRE(cache.pull(pair) == false);
-				REQUIRE(cache.peak(pair) == false);
-			}
-
-			SECTION("Size = 2")
-			{
-				pair.first = 2;
-				pair.second = "second";
-				REQUIRE(cache.push(pair) == true);
-				pair.second = "";
-				REQUIRE(cache.peak(pair) == true);
-				REQUIRE(pair.second == "second");
-				REQUIRE(cache.push(pair) == true);
-
-				REQUIRE(cache.capacity() == 2);
-				REQUIRE(cache.size() == 2);
+				REQUIRE(cache.size() == 1);
 				REQUIRE(cache.empty() == false);
-				REQUIRE(cache.full() == true);
-
-				pair.first = 3;
-				pair.second = "third";
-				REQUIRE(cache.push(pair) == false);
-
-				SECTION("Size = 1")
-				{
-					pair.first = 0;
-					pair.second = "";
-					REQUIRE(cache.pull(pair) == true);
-					REQUIRE(pair.first == 1);
-					REQUIRE(pair.second == "first");
-
-					REQUIRE(cache.capacity() == 2);
-					REQUIRE(cache.size() == 1);
-					REQUIRE(cache.empty() == false);
-					REQUIRE(cache.full() == false);
-				}
+				REQUIRE(cache.full() == false);
 			}
 		}
 	}
